9d70fb082361931a8aeed7fd2fbe9edbdfcebccf
Helll-BoY <59738142+Helll-BoY@users.noreply.github.com>
2021-04-29 18:24:32 +0400

    Add files via upload

---
+++ Scripts/CarGo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGo : MonoBehaviour
+{
+    Vector3 movevec;
+    void Start()
+    {
+        movevec = new Vector3(1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movevec * Time.deltaTime * 100);
+    }
+}
