b8edc026eaeca0b513acef822c02f5f244888cbc
phuljkeee <73863422+phuljkeee@users.noreply.github.com>
2021-05-18 23:28:06 +0300

    Menu
    Added Scripts

---
+++ 2.0/Subway/Assets/UI/UIScripts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
